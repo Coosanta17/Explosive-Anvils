115491a77e454d98ecab4fab9063975c2ae8f9d5
Coosanta17 <coosantamapy@gmail.com>
2023-10-12 09:19:58 +1100

    init commit - upload from MCreator because I am not good enough to code it myself.

---
+++ src/main/java/net/coosanta/explosive_anvils/ExplosiveAnvilsMod.java
@@ -0,0 +1,29 @@
+/*
+ *	MCreator note:
+ *
+ *	If you lock base mod element files, you can edit this file and the proxy files
+ *	and they won't get overwritten. If you change your mod package or modid, you
+ *	need to apply these changes to this file MANUALLY.
+ *
+ *
+ *	If you do not lock base mod element files in Workspace settings, this file
+ *	will be REGENERATED on each build.
+ *
+ */
+package net.coosanta.explosive_anvils;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExplosiveAnvilsMod implements ModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger();
+	public static final String MODID = "explosive_anvils";
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Initializing ExplosiveAnvilsMod");
+
+	}
+}
