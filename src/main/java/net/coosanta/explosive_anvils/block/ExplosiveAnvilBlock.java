92d0cc8c44e29f91bdc8029c2363d64438171e2e
Coosanta17 <coosantamapy@gmail.com>
2023-10-12 19:12:09 +1100

    Added explosive anvil recipe

--- src/main/java/net/coosanta/explosive_anvils/block/ExplosiveAnvilBlock.java
+++ src/main/java/net/coosanta/explosive_anvils/block/ExplosiveAnvilBlock.java
@@ -5,6 +5,7 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.material.FluidState;
@@ -22,10 +23,15 @@ import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.network.chat.Component;
 import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
@@ -36,6 +42,7 @@ import net.fabricmc.api.Environment;
 import net.fabricmc.api.EnvType;
 
 import net.coosanta.explosive_anvils.procedures.SpawnFallingExplosiveAnvilProcedure;
+import net.coosanta.explosive_anvils.procedures.ExplodeFallingExplosiveAnvilProcedure;
 import net.coosanta.explosive_anvils.init.ExplosiveAnvilsModBlocks;
 
 import java.util.List;
@@ -121,11 +128,39 @@ public class ExplosiveAnvilBlock extends Block implements SimpleWaterloggedBlock
 	@Override
 	public void neighborChanged(BlockState blockstate, Level world, BlockPos pos, Block neighborBlock, BlockPos fromPos, boolean moving) {
 		super.neighborChanged(blockstate, world, pos, neighborBlock, fromPos, moving);
+		if (world.getBestNeighborSignal(pos) > 0) {
+			ExplodeFallingExplosiveAnvilProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ());
+		}
 		SpawnFallingExplosiveAnvilProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ());
+	}
+
+	@Override
+	public void wasExploded(Level world, BlockPos pos, Explosion e) {
+		super.wasExploded(world, pos, e);
+		ExplodeFallingExplosiveAnvilProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ());
+	}
+
+	@Override
+	public void onProjectileHit(Level world, BlockState blockstate, BlockHitResult hit, Projectile entity) {
+		ExplodeFallingExplosiveAnvilProcedure.execute(world, hit.getBlockPos().getX(), hit.getBlockPos().getY(), hit.getBlockPos().getZ());
+	}
+
+	@Override
+	public InteractionResult use(BlockState blockstate, Level world, BlockPos pos, Player entity, InteractionHand hand, BlockHitResult hit) {
+		super.use(blockstate, world, pos, entity, hand, hit);
+		int x = pos.getX();
+		int y = pos.getY();
+		int z = pos.getZ();
+		double hitX = hit.getLocation().x;
+		double hitY = hit.getLocation().y;
+		double hitZ = hit.getLocation().z;
+		Direction direction = hit.getDirection();
+		ExplodeFallingExplosiveAnvilProcedure.execute(world, x, y, z);
+		return InteractionResult.SUCCESS;
 	}
 
 	@Environment(EnvType.CLIENT)
 	public static void clientInit() {
-		BlockRenderLayerMap.INSTANCE.putBlock(ExplosiveAnvilsModBlocks.EXPLOSIVE_ANVIL, RenderType.translucent());
+		BlockRenderLayerMap.INSTANCE.putBlock(ExplosiveAnvilsModBlocks.EXPLOSIVE_ANVIL, RenderType.solid());
 	}
 }
--- src/main/java/net/coosanta/explosive_anvils/init/ExplosiveAnvilsModProcedures.java
+++ src/main/java/net/coosanta/explosive_anvils/init/ExplosiveAnvilsModProcedures.java
@@ -5,10 +5,12 @@
 package net.coosanta.explosive_anvils.init;
 
 import net.coosanta.explosive_anvils.procedures.SpawnFallingExplosiveAnvilProcedure;
+import net.coosanta.explosive_anvils.procedures.ExplodeFallingExplosiveAnvilProcedure;
 
 @SuppressWarnings("InstantiationOfUtilityClass")
 public class ExplosiveAnvilsModProcedures {
 	public static void load() {
+		new ExplodeFallingExplosiveAnvilProcedure();
 		new SpawnFallingExplosiveAnvilProcedure();
 	}
 }
