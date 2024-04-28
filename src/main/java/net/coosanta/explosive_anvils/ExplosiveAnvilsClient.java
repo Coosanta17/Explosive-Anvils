27b0a48d1e2c11caab04d80e50ab8599ab76ea89
Coosanta17 <coosantamapy@gmail.com>
2024-04-28 16:37:32 +1000

    Customised setup, no content yet.

---
+++ src/main/java/net/coosanta/explosive_anvils/ExplosiveAnvilsClient.java
@@ -0,0 +1,10 @@
+package net.coosanta.explosive_anvils;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExplosiveAnvilsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
